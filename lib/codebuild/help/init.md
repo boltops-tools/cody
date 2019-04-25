ea172e1104a9b197e58dcfd052bbf79126f22c2f
Tung Nguyen <tongueroo@gmail.com>
2019-04-25 20:26:48 +0000

    infer project github url

---
+++ lib/codebuild/help/init.md
@@ -0,0 +1,4 @@
+## Examples
+
+    codebuild init # infers the name from the parent folder
+    codebuild init --name demo-codebuild-project # set the name
--- lib/codebuild/init.rb
+++ lib/codebuild/init.rb
@@ -3,6 +3,7 @@ module Codebuild
     # Ugly, this is how I can get the options from to match with this Thor::Group
     def self.cli_options
       [
+        [:name, desc: "CodeBuild project name."],
         [:force, type: :boolean, desc: "Bypass overwrite are you sure prompt for existing files."],
         [:template, desc: "Custom template to use."],
         [:template_mode, desc: "Template mode: replace or additive."],
@@ -13,6 +14,21 @@ module Codebuild
     def copy_project
       puts "Initialize codebuild project in .codebuild"
       directory "."
+    end
+
+  private
+    def project_name
+      inferred_name = File.basename(Dir.pwd).gsub('_','-').gsub(/[^0-9a-zA-Z,-]/, '')
+      @options[:name] || inferred_name
+    end
+
+    def project_github_url
+      default = "https://github.com/user/repo"
+      return default unless File.exist?(".git/config") && git_installed?
+
+      url = `git config --get remote.origin.url`.strip
+      url = url.sub('git@github.com:','https://github.com/')
+      url == '' ? default : url
     end
   end
 end
