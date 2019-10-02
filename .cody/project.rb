a72e8f652779d089031fd1734ac951ebd7e5d474
Tung Nguyen <tongueroo@gmail.com>
2019-10-02 00:39:49 +0000

    update repo

--- .cody/project.rb
+++ .cody/project.rb
@@ -1,4 +1,4 @@
-github_url("https://github.com/tongueroo/codebuild")
+github_url("https://github.com/tongueroo/cody")
 linux_image("aws/codebuild/ruby:2.5.3-1.7.0")
 triggers(webhook: true)
 local_cache(false)
