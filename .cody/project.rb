a6647cd2ed59a4ed177c8fc238d1333b25582cc9
Tung Nguyen <tongueroo@gmail.com>
2019-11-28 05:37:41 +0000

    only start build on push, no PR events

--- .cody/project.rb
+++ .cody/project.rb
@@ -1,4 +1,7 @@
 github_url("https://github.com/tongueroo/cody")
 linux_image("aws/codebuild/ruby:2.5.3-1.7.0")
-triggers(webhook: true)
+triggers(
+  webhook: true,
+  filter_groups: [[{type: "EVENT", pattern: "PUSH"}]]
+)
 local_cache(false)
