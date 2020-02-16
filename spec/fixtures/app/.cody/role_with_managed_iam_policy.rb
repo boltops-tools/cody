febdc1bafdd51d8303d641f99d259a9c5b0253c9
Prashanth <hi@prash.xyz>
2020-02-16 13:24:44 +0800

    test: role ManagedPolicyArns

---
+++ spec/fixtures/app/.cody/role_with_managed_iam_policy.rb
@@ -0,0 +1,1 @@
+managed_iam_policy('AmazonSNSReadOnlyAccess')
--- spec/lib/role_spec.rb
+++ spec/lib/role_spec.rb
@@ -1,12 +1,57 @@
 describe Cody::Role do
-  let(:role) do
-    Cody::Role.new(role_path: "spec/fixtures/app/.cody/role.rb")
-  end
+  let(:role) { Cody::Role.new(role_path: role_path) }
+  let(:role_path) { "spec/fixtures/app/.cody/role.rb" }
+  subject(:template) { role.run }
+
   context "general" do
     it "builds up the template in memory" do
-      template = role.run
       expect(template.keys).to eq ["IamRole"]
       expect(template["IamRole"]).to be_a(Hash)
+    end
+
+    it "sets default properties" do
+      expect(template["IamRole"]["Properties"]["Path"]).to eq("/")
+      expect(template["IamRole"]["Properties"]["AssumeRolePolicyDocument"]).to eq({
+        "Statement" => [{
+          "Action" => ["sts:AssumeRole"],
+          "Effect" => "Allow",
+          "Principal" => { "Service" => ["codebuild.amazonaws.com"]
+          }
+        }],
+        "Version" => "2012-10-17"
+      })
+      expect(template["IamRole"]["Properties"]["ManagedPolicyArns"]).to eq([
+        "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"])
+    end
+  end
+
+  context "empty managed_policy_arns value" do
+    let(:role_path) { "spec/fixtures/app/.cody/role_empty_managed_policy_arns.rb" }
+    it "sets ManagedPolicyArns to empty" do
+      expect(template["IamRole"]["Properties"]["ManagedPolicyArns"]).to eq([])
+    end
+  end
+
+  context "empty managed_iam_policy" do
+    let(:role_path) { "spec/fixtures/app/.cody/role_empty_managed_iam_policy.rb" }
+    it "sets default ManagedPolicyArns" do
+      expect(template["IamRole"]["Properties"]["ManagedPolicyArns"]).to eq([])
+    end
+  end
+
+  context "with managed_policy_arns value" do
+    let(:role_path) { "spec/fixtures/app/.cody/role_with_managed_policy_arns.rb" }
+    it "sets ManagedPolicyArns to value" do
+      expect(template["IamRole"]["Properties"]["ManagedPolicyArns"]).to eq([
+        "arn:aws:iam::aws:policy/AmazonSNSReadOnlyAccess"])
+    end
+  end
+
+  context "with managed_iam_policy value" do
+    let(:role_path) { "spec/fixtures/app/.cody/role_with_managed_iam_policy.rb" }
+    it "sets ManagedPolicyArns to value" do
+      expect(template["IamRole"]["Properties"]["ManagedPolicyArns"]).to eq([
+        "arn:aws:iam::aws:policy/AmazonSNSReadOnlyAccess"])
     end
   end
 end
