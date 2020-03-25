06339dcc2b4d39328faef9e93498eceef5ee9c89
Tung Nguyen <tongueroo@gmail.com>
2020-03-25 02:19:29 +0000

    add badge command

---
+++ lib/cody/badge.rb
@@ -0,0 +1,19 @@
+module Cody
+  class Badge < Base
+    def run
+      resp = codebuild.batch_get_projects(names: [@full_project_name])
+      project = resp.projects.first
+      unless project
+        puts "Project not found: #{@full_project_name}"
+        return
+      end
+
+      url = project.badge.badge_request_url
+      if @options[:markdown]
+        puts "![CodeBuild](#{url})"
+      else
+        puts url
+      end
+    end
+  end
+end
